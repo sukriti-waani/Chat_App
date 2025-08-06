85984df80aae3666e940300387f4a4dc1f1ce01f
unknown <sukritiwaani2006@gmail.com>
2025-08-06 22:39:27 +0530

    Refactor user list rendering in Sidebar with status and dynamic styling

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import assets from "../assets/assets";
+import assets, { userDummyData } from "../assets/assets";
 
 const Sidebar = ({ selectedUser, setSelectedUser }) => {
   const navigate = useNavigate();
@@ -39,6 +39,39 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
             placeholder="Search User..."
           />
         </div>
+      </div>
+
+      <div className="flex flex-col">
+        {userDummyData.map((user, index) => (
+          <div
+            onClick={() => {
+              setSelectedUser(user);
+            }}
+            key={index}
+            className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${
+              selectedUser?._id === user._id && "bg-[#282142]/50"
+            }`}
+          >
+            <img
+              src={user?.profilePic || assets.avatar_icon}
+              alt=""
+              className="w-[35px] aspect-[1/1] rounded-full"
+            />
+            <div className="flex flex-col leading-5">
+              <p>{user.fullName}</p>
+              {index < 3 ? (
+                <span className="text-green-400 text-xs">Online</span>
+              ) : (
+                <span className="text-neutral-400 text-xs">Offline</span>
+              )}
+            </div>
+            {index > 2 && (
+              <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
+                {index}
+              </p>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
