5554dcfbcaa0d79d377e9962cf31ce561c2ff1f0
unknown <sukritiwaani2006@gmail.com>
2025-08-16 23:27:15 +0530

    Database connection

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 import cors from "cors";
 import "dotenv/config";
-import express from 'express';
+import express from "express";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
 // Create Express app and HTTP server
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 // Middleware setup
-app.use(express.json({limit: "4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("Server is live"));
 
+// Connect to MongoDB
+await connectDB();
+
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on PORT:" + PORT))
+server.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
